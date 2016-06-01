4fcf1031d3df4003c86fed002dc971e134c2b75c
Tim Creasy <timcreasy@me.com>
2016-06-01 10:52:22 -0500

    Added List View and Add View logic

--- js/songs.js
+++ js/songs.js
@@ -1,3 +1,11 @@
+// =========================== DOM ELEMENTS ============================= //
+var songDisplayContainer = document.getElementById("songOutput");
+var addMusicButton = document.getElementById("addMusicButton");
+var listMusicButton = document.getElementById("listMusicButton");
+var addMusicView = document.getElementById("addMusicView");
+var listMusicView = document.getElementById("listMusicView");
+
+
 // Default songs
 
 var songs = [];
@@ -13,9 +21,6 @@ songs.unshift("Girlfriend Is Better - by Talking Heads on the album Stop Making 
 // Add song to end of array
 songs.push("Time - by Pink Floyd on the album The Dark Side of The Moon");
 
-// Get DOM element to add songs to
-var display = document.getElementById("songOutput");
-
 // Loop over array, removing any charachters that don't belong
 for (var i = 0; i < songs.length; i++) {
   // Current song being looked at
@@ -28,5 +33,23 @@ for (var i = 0; i < songs.length; i++) {
   // Store formatted song back in array
   songs[i] = currentSong;
 
-  display.innerHTML += `<p>${currentSong}</p>`;
+  songDisplayContainer.innerHTML += `<p>${currentSong}</p>`;
 }
+
+// =========================== FUNCTIONS ============================= //
+
+function addMusicPressed() {
+  listMusicView.classList.add("hidden");
+  addMusicView.classList.remove("hidden");
+}
+
+function listMusicPressed() {
+  addMusicView.classList.add("hidden");
+  listMusicView.classList.remove("hidden");
+}
+
+
+// =========================== EVENT LISTENERS ============================= //
+addMusicButton.addEventListener("click", addMusicPressed);
+listMusicButton.addEventListener("click", listMusicPressed);
+
