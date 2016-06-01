95b4da11ffb8aafcfb8eabe56fd018e6c041f557
Tim Creasy <timcreasy@me.com>
2016-06-01 11:06:05 -0500

    Added logic to output newly added songs to DOM

--- js/songs.js
+++ js/songs.js
@@ -4,12 +4,19 @@ var addMusicButton = document.getElementById("addMusicButton");
 var listMusicButton = document.getElementById("listMusicButton");
 var addMusicView = document.getElementById("addMusicView");
 var listMusicView = document.getElementById("listMusicView");
+var addButton = document.getElementById("addButton");
+var songInput = document.getElementById("songInput");
+var artistInput = document.getElementById("artistInput");
+var albumInput = document.getElementById("albumInput");
 
+// =========================== GLOBAL VARS ============================= //
 
-// Default songs
-
+// Songs array
 var songs = [];
 
+// =========================== DEFAULT SONGS ============================= //
+
+// Add songs to songs array
 songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
 songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
 songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
@@ -21,26 +28,34 @@ songs.unshift("Girlfriend Is Better - by Talking Heads on the album Stop Making 
 // Add song to end of array
 songs.push("Time - by Pink Floyd on the album The Dark Side of The Moon");
 
-// Loop over array, removing any charachters that don't belong
-for (var i = 0; i < songs.length; i++) {
-  // Current song being looked at
-  var currentSong = songs[i];
-
-  // Get rid of all astericks
-  currentSong = currentSong.replace(/[*@!(]/g, '');
-  // Replace > with -
-  currentSong = currentSong.replace(/[>]/g, '-');
-  // Store formatted song back in array
-  songs[i] = currentSong;
-
-  songDisplayContainer.innerHTML += `<p>${currentSong}</p>`;
-}
+// Output initial songs
+outputSongs();
 
 // =========================== FUNCTIONS ============================= //
+
+function outputSongs() {
+
+  // Loop over array, removing any charachters that don't belong
+  for (var i = 0; i < songs.length; i++) {
+    // Current song being looked at
+    var currentSong = songs[i];
+
+    // Get rid of all astericks
+    currentSong = currentSong.replace(/[*@!(]/g, '');
+    // Replace > with -
+    currentSong = currentSong.replace(/[>]/g, '-');
+    // Store formatted song back in array
+    songs[i] = currentSong;
+
+    songDisplayContainer.innerHTML += `<p>${currentSong}</p>`;
+  }
+
+}
 
 function addMusicPressed() {
   listMusicView.classList.add("hidden");
   addMusicView.classList.remove("hidden");
+  songInput.focus();
 }
 
 function listMusicPressed() {
@@ -48,8 +63,25 @@ function listMusicPressed() {
   listMusicView.classList.remove("hidden");
 }
 
+function addButtonPressed() {
+  // Build song string
+  var songString = songInput.value + " - by " + artistInput.value + " on the album " + albumInput.value;
+  // Add song to songs array
+  songs.push(songString);
+  // Clear current song list from DOM
+  songDisplayContainer.innerHTML = "";
+  // Reload song list
+  outputSongs();
+  // Go back to listView
+  listMusicPressed();
+  // Clear input fields
+  songInput.value = "";
+  artistInput.value = "";
+  albumInput.value = "";
+}
 
 // =========================== EVENT LISTENERS ============================= //
 addMusicButton.addEventListener("click", addMusicPressed);
 listMusicButton.addEventListener("click", listMusicPressed);
+addButton.addEventListener("click", addButtonPressed);
 
