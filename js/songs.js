96fff7124d11516a174738b1fe979c50661d3074
Tim Creasy <timcreasy@me.com>
2016-06-06 11:10:55 -0500

    Completed Music History v4

--- js/songs.js
+++ js/songs.js
@@ -9,79 +9,171 @@ var songInput = document.getElementById("songInput");
 var artistInput = document.getElementById("artistInput");
 var albumInput = document.getElementById("albumInput");
 
-// =========================== GLOBAL VARS ============================= //
+// =========================== SETUP ============================= //
 
 // Songs array
 var songs = [];
 
-// =========================== DEFAULT SONGS ============================= //
+// =========================== XHR CALLS ============================= //
 
-// Add songs to songs array
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-
-// Add song to beginning of array
-songs.unshift("Girlfriend Is Better - by Talking Heads on the album Stop Making Sense");
-// Add song to end of array
-songs.push("Time - by Pink Floyd on the album The Dark Side of The Moon");
-
-// Output initial songs
-outputSongs();
+// Load first list
+var xhrListOne = new XMLHttpRequest();
+xhrListOne.onreadystatechange = function() {
+  if(xhrListOne.readyState === 4 && xhrListOne.status === 200) {
+    var songData = JSON.parse(xhrListOne.responseText);
+    // Output songs for first list
+    outputSongs(songData, 1);
+  }
+};
+xhrListOne.open("GET", "../json/songList.json");
+xhrListOne.send();
 
 // =========================== FUNCTIONS ============================= //
 
-function outputSongs() {
+function deleteButtonPressed() {
+  // Get card element
+  var songCard = event.target.parentNode;
+  console.log(songCard);
+  // Remove from songDisplayContainer
+  songDisplayContainer.removeChild(songCard);
+}
 
-  // Loop over array, removing any charachters that don't belong
-  for (var i = 0; i < songs.length; i++) {
-    // Current song being looked at
-    var currentSong = songs[i];
+function moreButtonPressed() {
 
-    // Get rid of all astericks
-    currentSong = currentSong.replace(/[*@!(]/g, '');
-    // Replace > with -
-    currentSong = currentSong.replace(/[>]/g, '-');
-    // Store formatted song back in array
-    songs[i] = currentSong;
+  // Process second list
+  var xhrListTwo = new XMLHttpRequest();
+  xhrListTwo.onreadystatechange = function() {
+    if(xhrListTwo.readyState === 4 && xhrListTwo.status === 200) {
+      var songData = JSON.parse(xhrListTwo.responseText);
+      // Output songs for second list
+      outputSongs(songData, 2);
+    }
+  };
 
-    songDisplayContainer.innerHTML += `<p>${currentSong}</p>`;
+  xhrListTwo.open("GET", "../json/songList2.json");
+  xhrListTwo.send();
+}
+
+function outputSongs(songData, listNumber) {
+
+  // Check which list is being loaded
+  if (listNumber === 1) {
+
+    // Loop through each song
+    for (let i = 0; i < songData.songs.length; i++) {
+
+      // Get currentSong being looked at
+      let currentSong = songData.songs[i];
+
+      // Create song card container
+      let songCard = document.createElement("div");
+      // Add a class to container
+      songCard.class = "songCard";
+      // Create song card html
+      songCard.innerHTML = `<p class="songInformation">${currentSong.title} - ${currentSong.artist} - ${currentSong.album}</p>`;
+      // Create delete button
+      let deleteButton = document.createElement("button");
+      // Add class to delete button
+      deleteButton.class = "deleteButton";
+      // Add "Delete" to button text
+      deleteButton.innerHTML = "Delete";
+      // Add delete button to songCard
+      songCard.appendChild(deleteButton);
+      // Add event listener to delete button
+      deleteButton.addEventListener("click", deleteButtonPressed);
+      // Append entire songCard to songDisplayContainer
+      songDisplayContainer.appendChild(songCard);
+    
+    }
+
+    // When first list is done being added to DOM, add more button
+    // Create more button
+    let moreButton = document.createElement("button");
+    // Add ID to moreButton
+    moreButton.id = "moreButton";
+    // Add text to button
+    moreButton.innerHTML = "More";
+    // Add event listener to more button
+    moreButton.addEventListener("click", moreButtonPressed);
+    // Append to songDisplayContainer
+    songDisplayContainer.appendChild(moreButton);
+
+
+  } else if (listNumber === 2) {
+
+    // First remove moreButton
+    let moreButton = document.getElementById("moreButton");
+    songDisplayContainer.removeChild(moreButton);
+
+    // Loop through each song
+    for (let i = 0; i < songData.songs.length; i++) {
+
+      // Get currentSong being looked at
+      let currentSong = songData.songs[i];
+
+      // Create song card container
+      let songCard = document.createElement("div");
+      // Add a class to container
+      songCard.class = "songCard";
+      // Create song card html
+      songCard.innerHTML = `<p class="songInformation">${currentSong.title} - ${currentSong.artist} - ${currentSong.album}</p>`;
+      // Create delete button
+      let deleteButton = document.createElement("button");
+      // Add class to delete button
+      deleteButton.class = "deleteButton";
+      // Add "Delete" to button text
+      deleteButton.innerHTML = "Delete";
+      // Add delete button to songCard
+      songCard.appendChild(deleteButton);
+      // Add event listener to delete button
+      deleteButton.addEventListener("click", deleteButtonPressed);
+      // Append entire songCard to songDisplayContainer
+      songDisplayContainer.appendChild(songCard);
+    
+    }
+    
   }
 
 }
 
+// Add music button pressed
 function addMusicPressed() {
+  // Hide List View
   listMusicView.classList.add("hidden");
+  // Remove hidden class from addMusicView, so it is showing
   addMusicView.classList.remove("hidden");
+  // Set focus on text input
   songInput.focus();
 }
 
+// List view button pressed
 function listMusicPressed() {
+  // Hide Add Music View
   addMusicView.classList.add("hidden");
+  // Remove hidden class from listMusicView, so it is showing
   listMusicView.classList.remove("hidden");
 }
 
-function addButtonPressed() {
-  // Build song string
-  var songString = songInput.value + " - by " + artistInput.value + " on the album " + albumInput.value;
-  // Add song to songs array
-  songs.push(songString);
-  // Clear current song list from DOM
-  songDisplayContainer.innerHTML = "";
-  // Reload song list
-  outputSongs();
-  // Go back to listView
-  listMusicPressed();
-  // Clear input fields
-  songInput.value = "";
-  artistInput.value = "";
-  albumInput.value = "";
-}
+// // Add song to songs array
+// function addButtonPressed() {
+//   // Build song string
+//   var songString = songInput.value + " - by " + artistInput.value + " on the album " + albumInput.value;
+//   // Add song to songs array
+//   songs.push(songString);
+//   // Clear current song list from DOM
+//   songDisplayContainer.innerHTML = "";
+//   // Reload song list
+//   outputSongs();
+//   // Go back to listView
+//   listMusicPressed();
+//   // Clear input fields
+//   songInput.value = "";
+//   artistInput.value = "";
+//   albumInput.value = "";
+// }
 
 // =========================== EVENT LISTENERS ============================= //
 addMusicButton.addEventListener("click", addMusicPressed);
 listMusicButton.addEventListener("click", listMusicPressed);
-addButton.addEventListener("click", addButtonPressed);
+//addButton.addEventListener("click", addButtonPressed);
 
